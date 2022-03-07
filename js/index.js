dcce9a2b70da35c13563a612717705b9dcfe4a5d
AtienoObwanda <atienoobwanda@gmail.com>
2022-03-07 14:52:03 +0300

    Fixed the day calculation bug

--- js/index.js
+++ js/index.js
@@ -1,23 +1,28 @@
+var fNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]; //FEMALE NAMES
+var maleNames = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]; //MALE NAMES
 
-var akan = function(y, m, d, g) {
-    var femNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-    var maleNames = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-    var d = new Date(y, --m, d);
-    if (g === "Female") {
-        return d && femNames[d.getDay()];
+
+
+var akanName = function( year,month,day,gender) { //FUNCTION FOR CALCULATING THE AKAN NAME
+    
+    var day= new Date(`${year}-${month}-${day}`)
+    if (gender === "Female") {
+        return day && fNames[day.getDay()];
     } else {
-        return d && maleNames[d.getDay()];
+        return day && maleNames[day.getDay()];
     }
 }
+
+//function to get the details entered on the form
 $(document).ready(function() {
     $("form#form").submit(function(event) {
-        event.preventDefault();
-        var y = parseInt($("#year").val());
-        var m = parseInt($("#month").val());
-        var d = parseInt($("#date").val());
-        var g = $("input:radio[name=gender]:checked").val();
-        var result = akan(y, m, d, g);
-        alert("Hello, your akan name is: " + result);
+        event.preventDefault(); //prevents default reload!
+        var year = parseInt($("#year").val()); //Same as document.getElementById ("year").value;
+        var month = parseInt($("#month").val());
+        var day = parseInt($("#date").val());
+        var gender = $("input:radio[name=gender]:checked").val();
+        var akanDay = akanName(year, month, day, gender);
+        alert("Your akan name is: " + akanDay);
         //refresh page
         document.getElementById("form").reset();
     });
